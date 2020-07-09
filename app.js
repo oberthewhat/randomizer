bc17477cd298fb3dad23fa8c3bf286ca4d24310c
John Obertubbesing <oberthewhat@gmail.com>
2020-07-08 22:28:44 -0500

    finish first notes

--- app.js
+++ app.js
@@ -35,14 +35,12 @@ const randomNumGenerator = function () {
 
 function appendRandItem() {
   let item = list[randomNumGenerator()]; //pass the random number to get the item index
-  let elem = document.createElement("p"); //gets element of p and stores as elem
+	let elem = document.createElement("p"); //gets element of p and stores as elem
+  let selectedItemId = item.replace(/\s+/g, "");	
   elem.innerHTML = item; //makes the element display the text of the item
-  elem.setAttribute("id", item);// sets the id as the item without spaces
-  randomItemDisplay.appendChild(elem);
-
-  let selectedItemClass = item.replace(/\s+/g, "");
-  console.log(selectedItemClass);
-  removeSelectedItem(selectedItemClass, item);
+  elem.setAttribute("id", selectedItemId);// sets the id as the item without spaces
+  randomItemDisplay.appendChild(elem); 
+  removeSelectedItem(selectedItemClass, item);//function that removes from dom and array
 }
 
 function removeSelectedItem(idName, text) {
